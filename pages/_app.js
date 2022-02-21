bae98bab736dd7f2e44b6bb2c30421fdf24f0294
Conor Kennedy <conor@conorkennedy.com>
2022-02-21 07:59:07 -0500

    started home page

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
 import '../styles/globals.css'
-
+import React from 'react';
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+  <React.Fragment>
+    <Component {...pageProps} />
+    <footer>Conor 2021</footer>
+  </React.Fragment>
+  );
 }
 
 export default MyApp
